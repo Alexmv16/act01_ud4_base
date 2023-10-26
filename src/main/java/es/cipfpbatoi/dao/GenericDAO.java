3929a6ac0e0318686500f3252bc73cafb3339f20
Sergio <s.galisteocastro@edu.gva.es>
2023-10-26 20:16:40 +0200

    primera version

---
+++ src/main/java/es/cipfpbatoi/dao/GenericDAO.java
@@ -0,0 +1,23 @@
+package es.cipfpbatoi.dao;
+import java.util.List;
+
+public interface GenericDAO<T> {
+	T find(int id) throws Exception;
+
+	List<T> findAll() throws Exception;
+
+	T insert(T t) throws Exception;
+
+	boolean update(T t) throws Exception;
+	
+	boolean save(T t) throws Exception;
+
+	// boolean delete(int id) throws Exception;
+
+	boolean delete(T t) throws Exception;
+
+	long size() throws Exception;;
+
+	List<T> findByExample(T t) throws Exception;
+}
+
